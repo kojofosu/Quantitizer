0b51cf840458f4ebd9500c5d5a273a9f0ebc9b50
Kojo Fosu Bempa Edue <kojofosube@gmail.com>
2021-07-17 15:21:11 +0000

    Adds animation

--- QuantitizerLibrary/src/main/java/com/mcdev/quantitizerlibrary/HorizontalQuantitizer.kt
+++ QuantitizerLibrary/src/main/java/com/mcdev/quantitizerlibrary/HorizontalQuantitizer.kt
@@ -1,18 +1,26 @@
 package com.mcdev.quantitizerlibrary
 
+import android.animation.ObjectAnimator
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.daasuu.ei.Ease
+import com.daasuu.ei.EasingInterpolator
 import com.mcdev.quantitizerlibrary.databinding.ActivityHorizontalQuantitizerBinding
+import java.util.*
 
 class HorizontalQuantitizer @JvmOverloads constructor(context: Context,
                                                       attributeSet: AttributeSet? = null ,
                                                       defStyle: Int = 0):
     ConstraintLayout(context, attributeSet, defStyle){
 
+    private val DURATION = 300L
     private val binding = ActivityHorizontalQuantitizerBinding.inflate(LayoutInflater.from(context), this, true)
     private var currentValue: Int = 0
 
@@ -24,22 +32,55 @@ class HorizontalQuantitizer @JvmOverloads constructor(context: Context,
 
         /*increase*/
         binding.increaseIb.setOnClickListener {
-            binding.increaseIb.setOnClickListener {
-                doInc()
-            }
+            doInc()
         }
     }
 
     private fun doInc() {
+        animateInc()
         val increasedValue: Int = currentValue.inc()
-        binding.quantityTv.text = increasedValue.toString()
         currentValue = increasedValue
+        animateNextInc()
     }
 
     private fun doDec() {
+        animateDec()
         val decreasedValue: Int = currentValue.dec()
-        binding.quantityTv.text = decreasedValue.toString()
         currentValue = decreasedValue
+        animateNextDec()
     }
 
+    private fun animateInc() {
+        val animator = ObjectAnimator.ofFloat(binding.quantityTv, "translationX", 0f, 200f);
+        animator.interpolator = EasingInterpolator(Ease.BACK_IN)
+        animator.start()
+    }
+
+    private fun animateNextInc() {
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                binding.quantityTv.text = currentValue.toString()
+
+                val animator = ObjectAnimator.ofFloat(binding.quantityTv, "translationX", 200f, 0f);
+                animator.interpolator = EasingInterpolator(Ease.BACK_OUT)
+                animator.start()
+            }, DURATION)
+    }
+
+    private fun animateDec() {
+        val animator = ObjectAnimator.ofFloat(binding.quantityTv, "translationX", 0f, -200f);
+        animator.interpolator = EasingInterpolator(Ease.BACK_IN)
+        animator.start()
+    }
+
+    private fun animateNextDec() {
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                binding.quantityTv.text = currentValue.toString()
+
+                val animator = ObjectAnimator.ofFloat(binding.quantityTv, "translationX", -200f, 0f);
+                animator.interpolator = EasingInterpolator(Ease.BACK_OUT)
+                animator.start()
+            }, DURATION)
+    }
 }
